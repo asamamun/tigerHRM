1171e1635459e576f90af06a0a6198a6d568de32
Imran Islam <imranislamp@gmail.com>
2022-06-25 02:40:01 +0600

    Dashboard page updated

---
+++ app/Views/auth/login3.php
@@ -0,0 +1,106 @@
+<?=view('partial/header')?>
+   <div class="container">
+
+<!-- flash message view -->
+<?php echo view("partial/flashmessage");?>
+<section class="vh-100">
+  <div class="container-fluid h-custom">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-9 col-lg-6 col-xl-5">
+        <img src="<?= base_url() ?>/assets/images/draw2.webp"
+          class="img-fluid" alt="Sample image">
+      </div>
+      <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+        <?php echo form_open("login"); ?>
+          <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+            <p class="lead fw-normal mb-0 me-3">Sign in with</p>
+            <button type="button" class="btn btn-primary btn-floating mx-1">
+              <i class="fab fa-facebook-f"></i>
+            </button>
+
+            <button type="button" class="btn btn-primary btn-floating mx-1">
+              <i class="fab fa-twitter"></i>
+            </button>
+
+            <button type="button" class="btn btn-primary btn-floating mx-1">
+              <i class="fab fa-linkedin-in"></i>
+            </button>
+          </div>
+
+          <div class="divider d-flex align-items-center my-4">
+            <p class="text-center fw-bold mx-3 mb-0">Or</p>
+          </div>
+
+          <!-- Email input -->
+          <div class="form-outline mb-4">
+            <input type="email" id="email" name="email" class="form-control form-control-lg"
+              placeholder="Enter a valid email address" value="<?= old('email') ?>"  />
+            <label class="form-label" for="email">Email address</label>
+          </div>
+
+          <!-- Password input -->
+          <div class="form-outline mb-3">
+            <input type="password" id="password" name="password" class="form-control form-control-lg"
+              placeholder="Enter password" required />
+            <label class="form-label" for="password">Password</label>
+          </div>
+
+          <div class="d-flex justify-content-between align-items-center">
+            <!-- Checkbox -->
+            <div class="form-check mb-0">
+              <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+              <label class="form-check-label" for="form2Example3">
+                Remember me
+              </label>
+            </div>
+            <a href="#!" class="text-body">Forgot password?</a>
+          </div>
+
+          <div class="text-center text-lg-start mt-4 pt-2">
+            <button type="submit" class="btn btn-primary btn-lg"
+              style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
+            <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="register"
+                class="link-danger">Register</a></p>
+          </div>
+
+        <?php echo form_close() ?>
+      </div>
+    </div>
+  </div>
+  <div
+    class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+    <!-- Copyright -->
+    <div class="text-white mb-3 mb-md-0">
+      Copyright © 2020. All rights reserved.
+    </div>
+    <!-- Copyright -->
+
+    <!-- Right -->
+    <div>
+      <a href="#!" class="text-white me-4">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="#!" class="text-white me-4">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="#!" class="text-white me-4">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="#!" class="text-white">
+        <i class="fab fa-linkedin-in"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </div>
+</section>
+
+
+
+
+
+   </div>
+   <script src="<?php echo base_url()?>/assets/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo base_url()?>/assets/js/jquery-3.5.1.js"></script>
+</body>
+</html>
+
--- app/Views/dashboard/dashboard.php
+++ app/Views/dashboard/dashboard.php
@@ -3,8 +3,283 @@
 <!-- content section start -->
 <?= $this->section('content'); ?>
 <h1>Welcome to</h1>
-<h1>Dashboard Tiger HRM</h1>
+<!-- ===================================================================== -->
+      <div class="page-wrapper">
+            <div class="message"></div>
+            <div class="row page-titles">
+                <div class="col-md-5 align-self-center">
+                    <h3 class="text-themecolor"><i class="fa fa-braille" style="color:#1976d2"></i>&nbsp Dashboard</h3>
+                </div>
+                <div class="col-md-7 align-self-center">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+                        <li class="breadcrumb-item active">Dashboard</li>
+                    </ol>
+                </div>
+            </div>
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Row -->
+                <div class="row">
+                    <!-- Column -->
+                    <div class="col-lg-3 col-md-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-flex flex-row">
+                                    <div class="round align-self-center round-primary"><i class="ti-user"></i></div>
+                                    <div class="m-l-10 align-self-center">
+                                        <h3 class="m-b-0">
+                                    Employees</h3>
+                                        <a href="<?php echo base_url(); ?>employee/Employees" class="text-muted m-b-0">View Details</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Column -->
+                    <!-- Column -->
+                    <div class="col-lg-3 col-md-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-flex flex-row">
+                                    <div class="round align-self-center round-info"><i class="ti-file"></i></div>
+                                    <div class="m-l-10 align-self-center">
+                                        <h3 class="m-b-0">
+                                             Leaves
+                                        </h3>
+                                        <a href="<?php echo base_url(); ?>leave/Application" class="text-muted m-b-0">View Details</a>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Column -->
+                    <!-- Column -->
+                    <div class="col-lg-3 col-md-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-flex flex-row">
+                                    <div class="round align-self-center round-danger"><i class="ti-calendar"></i></div>
+                                    <div class="m-l-10 align-self-center">
+                                        <h3 class="m-b-0"> 
+                                        Projects
+                                        </h3>
+                                        <a href="<?php echo base_url(); ?>Projects/All_Projects" class="text-muted m-b-0">View Details</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Column -->
+                    <!-- Column -->
+                    <div class="col-lg-3 col-md-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-flex flex-row">
+                                    <div class="round align-self-center round-success"><i class="ti-money"></i></div>
+                                    <div class="m-l-10 align-self-center">
+                                        <h3 class="m-b-0">
+                                         Loan 
+                                        </h3>
+                                        <a href="<?php echo base_url(); ?>Loan/View" class="text-muted m-b-0">View Details</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Column -->
+                </div>
+                <!-- Row -->
+                <!-- Row -->
+                
+                <div class="row ">
+                    <!-- Column -->
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-inverse card-info">
+                            <div class="box bg-primary text-center">
+                                <h1 class="font-light text-white">
+                                  
+                                </h1>
+                                <h6 class="text-white">Former Employees</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Column -->
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-info card-inverse">
+                            <div class="box text-center">
+                                <h1 class="font-light text-white">
+                                          
+                                </h1>
+                                <h6 class="text-white">Pending Leave Application</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Column -->
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-inverse card-danger">
+                            <div class="box text-center">
+                                <h1 class="font-light text-white">
+                                    
+                                </h1>
+                                <h6 class="text-white">Upcoming Project</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Column -->
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-inverse card-success">
+                            <div class="box text-center">
+                                <h1 class="font-light text-white">
+                                        
+                                </h1>
+                                <h6 class="text-white">Loan Application</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Column -->
+                </div>
+                <!-- ============================================================== -->
+            </div> 
+            <div class="container-fluid">
+                <!-- ?php $notice = $this->notice_model->GetNoticelimit(); 
+                $running = $this->dashboard_model->GetRunningProject(); 
+                $userid = $this->session->userdata('user_login_id');
+                $todolist = $this->dashboard_model->GettodoInfo($userid);                 
+                $holiday = $this->dashboard_model->GetHolidayInfo();                  -->
+               
+                <!-- Row -->
+                <div class="row">
+                    
+                    <div class="col-md-8">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Running Project/s</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive" style="height:600px;overflow-y:scroll">
+                                    <table class="table table-bordered table-hover earning-box">
+                                        <thead>
+                                            <tr>
+                                                <th>Title</th>
+                                                <th>Start Date</th>
+                                                <th>End Date</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                           
+                                            <tr style="vertical-align:top;">
+                                                <td><a href=""></a></td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+                                            
+                                        </tbody>
+                                    </table>
+                                </div>                                
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Column -->
+                    <div class="col-lg-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">To Do list</h4>
+                                <h6 class="card-subtitle">List of your next task to complete</h6>
+                                <div class="to-do-widget m-t-20" style="height:550px;overflow-y:scroll">
+                                            <ul class="list-task todo-list list-group m-b-0" data-role="tasklist">
+                                               
+                                                <li class="list-group-item" data-role="task">
+                                                   
+                                                    <div class="checkbox checkbox-info">
+                                                        
+                                                    </div>
+                                                    
+                                                    <div class="checkbox checkbox-info">
+                                                        
+                                                    </div> 
+                                                                                                  
+                                                </li>
 
+                                            </ul>                                    
+                                </div>
+                                <div class="new-todo">
+                                   <form method="post" action="add_todo" enctype="multipart/form-data" id="add_todo" >
+                                    <div class="input-group">
+                                        <input type="text" name="todo_data" class="form-control" style="border: 1px solid #fff !IMPORTANT;" placeholder="Enter New Task...">
+                                        <span class="input-group-btn">
+                                        
+                                        <button type="submit" class="btn btn-success todo-submit"><i class="fa fa-plus"></i></button>
+                                        </span> 
+                                    </div>
+                                    </form>
+                                </div>                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Row -->
+                <div class="row">
+                    <div class="col-lg-8">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Notice Board</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive slimScrollDiv" style="height:600px;overflow-y:scroll">
+                                    <table class="table table-hover table-bordered earning-box ">
+                                        <thead>
+                                            <tr>
+                                                <th>Title</th>
+                                                <th>File</th>
+                                                <th>Date</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                          
+                                            <tr class="scrollbar" style="vertical-align:top">
+                                                
+                                            </tr>
+                                       
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    Holidays
+                                </h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive" style="height:600px;overflow-y:scroll">
+                                    <table class="table table-hover table-bordered earning-box">
+                                       <thead>
+                                            <tr>
+                                                <th>Holiday Name</th>
+                                                <th>Date</th>
+                                            </tr>                                           
+                                       </thead>
+                                       <tbody>
+                                          
+                                           <tr style="background-color:#e3f0f7">
+                                               
+                                           </tr>
+                                          
+                                       </tbody> 
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+                                             
+
+<!-- ===================================================================== -->
 
 <?= $this->endSection(); ?>
 <!-- content section end-->
