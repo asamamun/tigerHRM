9a53a27fb15b9bf96fdacfa2eaf60a8c2351de0e
A Mahmud Umer <94960783+umerthedev@users.noreply.github.com>
2022-06-25 00:27:38 +0600

    links updated

--- app/Views/partial/sidebar.php
+++ app/Views/partial/sidebar.php
@@ -29,7 +29,7 @@
                     <div class="sb-sidenav-menu">
                         <div class="nav">
                             <div class="sb-sidenav-menu-heading">Core</div>
-                            <a class="nav-link" href="index.html">
+                            <a class="nav-link" href="<?= base_url() ?>/dashboard">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Dashboard
                             </a>
