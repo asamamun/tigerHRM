5c3736b74542672c70bc6d30ef572831511810fb
asamamun <asamamun.web@gmail.com>
2022-07-06 19:00:08 +0600

    Auth.php created, in filters auth implemented. add routes in line 74 in filters.php for authorization

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -8,6 +8,7 @@ use CodeIgniter\Filters\DebugToolbar;
 use CodeIgniter\Filters\Honeypot;
 use CodeIgniter\Filters\InvalidChars;
 use CodeIgniter\Filters\SecureHeaders;
+use App\Filters\Auth;
 
 class Filters extends BaseConfig
 {
@@ -23,6 +24,7 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
+        'auth'     => Auth::class,
     ];
 
     /**
@@ -68,5 +70,7 @@ class Filters extends BaseConfig
      *
      * @var array
      */
-    public $filters = [];
+    public $filters = [
+        'auth'=> ['before' => ['department/*', 'employee/*','section/*','section']]
+    ];
 }
